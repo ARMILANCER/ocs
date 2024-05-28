b8bd5d645f663dc9e53c06c473ca9fff027aacb6
Lor3nzo-pixel <ctzlnz05e30e514t@isisbernocchi.edu.it>
2024-05-29 01:46:51 +0200

    ookok

--- game/js/padlock.js
+++ game/js/padlock.js
@@ -4,32 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Funzione per gestire il blocco/sblocco della pagina
     function togglePageLock() {
+        const container = document.querySelector('.div_container');
         if (isLocked) {
             // Blocca lo scorrimento verticale della pagina
+            container.classList.add('locked');
+
             button.style.backgroundImage = "url('../img/locked.svg')";
-            document.body.style.overflow = 'hidden';
-            document.documentElement.style.overflow = 'hidden';
-            // Riporta l'utente all'inizio della pagina
-            window.scrollTo({ top: 0, behavior: 'smooth' });
         } else {
             // Sblocca lo scorrimento verticale della pagina
+            container.classList.remove('locked');
+
             button.style.backgroundImage = "url('../img/unlocked.svg')";
-            document.body.style.overflow = '';
-            document.documentElement.style.overflow = '';
         }
     }
 
     button.addEventListener('click', function() {
         isLocked = !isLocked;
         togglePageLock();
-    });
-
-    // Blocca la pagina se il bottone è già bloccato quando la finestra viene ridimensionata o lo scorrimento avviene
-    window.addEventListener('resize', togglePageLock);
-    window.addEventListener('scroll', function() {
-        if (isLocked) {
-            // Impedisce lo scorrimento se la pagina è bloccata
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
     });
 });
