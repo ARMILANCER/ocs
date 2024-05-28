3919778415828c0926451ce569402c6426aaf9de
Lor3nzo-pixel <ctzlnz05e30e514t@isisbernocchi.edu.it>
2024-05-28 11:45:17 +0200

    asd

--- game/php/home.php
+++ game/php/home.php
@@ -3,7 +3,6 @@ include 'db_conn.php';
 
 $query_user = "SELECT * FROM `user` WHERE id = 1 || id = 60";
 $result_user = mysqli_query($con, $query_user);
-
 if ($result_user && mysqli_num_rows($result_user) > 0) {
     $user = mysqli_fetch_assoc($result_user);
     echo json_encode($user);
