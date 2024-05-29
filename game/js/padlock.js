3d015d63cc43b969646add302834f7ac4965fb21
christianbrito <arielbrito864@gmail.com>
2024-05-29 03:20:15 +0200

    asdasd

--- game/js/padlock.js
+++ game/js/padlock.js
@@ -2,22 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('but_padlock');
     let isLocked = false;
 
-    // Funzione per gestire il blocco/sblocco della pagina
     function togglePageLock() {
         const container = document.querySelector('.div_container');
         if (isLocked) {
-            // Blocca lo scorrimento verticale della pagina
             container.classList.add('locked');
-
             button.style.backgroundImage = "url('../img/locked.svg')";
         } else {
-            // Sblocca lo scorrimento verticale della pagina
             container.classList.remove('locked');
-
             button.style.backgroundImage = "url('../img/unlocked.svg')";
         }
     }
-
     button.addEventListener('click', function() {
         isLocked = !isLocked;
         togglePageLock();
--- game/php/db_conn.php
+++ game/php/db_conn.php
@@ -3,8 +3,4 @@ $con = mysqli_connect("localhost","root","root","ocs");
 if(!$con){
     trigger_error(mysqli_connect_error());
 }
-/*
-//DEBUG
-else{ echo("Good job"); }
-*/
 ?>
