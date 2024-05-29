3789ad754a02796f9e6e45b90c325954d9f033e6
christianbrito <arielbrito864@gmail.com>
2024-05-29 11:46:15 +0200

    asdasdasd

--- game/js/request.js
+++ game/js/request.js
@@ -3,11 +3,11 @@ function getScenarioData() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var scenarios = JSON.parse(this.responseText);
-      document.getElementById("h_title").innerHTML = scenarios[0].name;
-      document.getElementById("p_state").innerHTML = scenarios[0].description;
-      document.getElementById("div_img").style.backgroundImage = "url('" + scenarios[0].image + "')";
-      document.getElementById("p_choicex").innerHTML = scenarios[0].choiceY;
-      document.getElementById("p_choicey").innerHTML = scenarios[0].choiceY;
+      document.getElementById("h_title").innerHTML = scenarios[1].name;
+      document.getElementById("p_state").innerHTML = scenarios[1].description;
+      document.getElementById("div_img").style.backgroundImage = "url('" + scenarios[1].image + "')";
+      document.getElementById("p_choicex").innerHTML = scenarios[1].choiceY;
+      document.getElementById("p_choicey").innerHTML = scenarios[1].choiceY;
     }
   };
   xhttp.open("GET", "../php/request.php", true);
