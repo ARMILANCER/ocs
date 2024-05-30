1e73a0079556df371f56d3cb2ddada0ef49f7394
Lor3nzo-pixel <ctzlnz05e30e514t@isisbernocchi.edu.it>
2024-05-31 01:01:37 +0200

    we

--- game/js/music_player.js
+++ game/js/music_player.js
@@ -2,7 +2,7 @@ function playMusic() {
     var audio = document.getElementById('background-music');
 
     //audio.src = "../music/dinero.mp3";
-    audio.src = "../music/rollacoaster.mp3";
+    audio.src = "../music/main_soundtrack.mp3";
     audio.play().catch(function(error) {
         console.log('Autoplay prevented: ' + error);
     });
