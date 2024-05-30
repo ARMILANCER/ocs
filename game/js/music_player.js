cfe4afdd8fc7a819c83df2ca56a231b0475e1722
christianbrito <arielbrito864@gmail.com>
2024-05-31 01:06:02 +0200

    xzas

--- game/js/music_player.js
+++ game/js/music_player.js
@@ -1,6 +1,5 @@
 function playMusic() {
     var audio = document.getElementById('background-music');
-
     //audio.src = "../music/dinero.mp3";
     audio.src = "../music/main_soundtrack.mp3";
     audio.play().catch(function(error) {
