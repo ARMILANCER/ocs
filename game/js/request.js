aa762d9ca6d8aec548f266c7ad9e01168d39053b
christianbrito <arielbrito864@gmail.com>
2024-05-30 09:58:08 +0200

    hgmj

--- game/js/request.js
+++ game/js/request.js
@@ -1,19 +1,19 @@
+var i = 5;
 function getScenarioData() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var scenarios = JSON.parse(this.responseText);
-      document.getElementById("h_title").innerHTML = scenarios[2].name;
-      document.getElementById("p_state").innerHTML = scenarios[2].description;
-      document.getElementById("div_img").style.backgroundImage = "url('" + scenarios[2].image + "')";
-      document.getElementById("p_choicex").innerHTML = scenarios[2].choice1;
-      document.getElementById("p_choicey").innerHTML = scenarios[2].choice2;
+      document.getElementById("h_title").innerHTML = scenarios[i].name;
+      document.getElementById("p_state").innerHTML = scenarios[i].description;
+      document.getElementById("div_img").style.backgroundImage = "url('" + scenarios[i].image + "')";
+      document.getElementById("p_choicex").innerHTML = scenarios[i].choice1;
+      document.getElementById("p_choicey").innerHTML = scenarios[i].choice2;
     }
   };
   xhttp.open("GET", "../php/request.php", true);
   xhttp.send();
 }
-
 window.onload = function() {
   getScenarioData();
 };
